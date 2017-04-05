a06eb1095c6bb392fac02d7c2e84d6d913e2239a
Breana Tate <brt2109@columbia.edu>
2017-04-04 22:11:50 -0400

    testing

--- server.py
+++ server.py
@@ -113,10 +113,10 @@ def index():
   #
   # example of a database query
   #
-  cursor = g.conn.execute("SELECT name FROM individuals")
+  cursor = g.conn.execute("SELECT * FROM individuals")
   names = []
   for result in cursor:
-    names.append(result['name'])  # can also be accessed using result[0]
+    names.append(result[0.name])  # can also be accessed using result[0]
   cursor.close()
 
   #
@@ -146,6 +146,7 @@ def index():
   #     {% endfor %}
   #
   context = dict(data = names)
+  
 
 
   #
