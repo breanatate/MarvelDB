8be9e9d50ec9abdecf29e50aa0de968df6133624
Breana Tate <brt2109@columbia.edu>
2017-04-05 00:43:09 -0400

    fixed error

--- server.py
+++ server.py
@@ -113,10 +113,10 @@ def index():
   #
   # example of a database query
   #
-  cursor = g.conn.execute("SELECT * FROM individuals")
+  cursor = g.conn.execute("SELECT name FROM individuals")
   names = []
   for result in cursor:
-    names.append(result[0.name])  # can also be accessed using result[0]
+    names.append(result['name'])  # can also be accessed using result[0]
   cursor.close()
 
   #
