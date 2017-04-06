8b000c12f53fc3ef6d5787761aadeabafc7c6f68
Breana Tate <brt2109@columbia.edu>
2017-04-06 14:43:49 -0400

    server changes

--- server.py
+++ server.py
@@ -162,8 +162,8 @@ def index():
   #
   context = dict(data = anames)
  
-  context2= dict(data2 = unis)
-  return render_template("index.html", **context2)
+  context= dict(data2 = unis)
+  #return render_template("index.html", **context2)
   
   context= dict(data3 = powers)
 
@@ -173,7 +173,7 @@ def index():
   # render_template looks in the templates/ folder for files.
   # for example, the below file reads template/index.html
   #
-  return render_template("index.html", **context)
+  return render_template("index.html")
   #return render_template("index.html")
 
 
